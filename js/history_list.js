649646e8cc927baf3f6fade7849a7ca8b7e7f58c
Anna Grochola <grochola.anna@gmail.com>
2016-09-07 00:40:50 +0200

    html structure grywalizacja

--- js/history_list.js
+++ js/history_list.js
@@ -18,15 +18,40 @@ $(document).ready(function() {
 
 
     ajaxPost(null, "data/history", "GET", function (msg) {
+            console.log("Poniżej jest msg:");
             console.log(msg);
 
-        for (i=0; i < msg.content.length; i++) {       //wchodzę do obiektu z API, do jego atrybutu content, w którym jest tablica
+            console.log("poniżej jest tablica msg.content z obiektami:");
+            console.log(msg.content);
 
-            console.log(msg.content[i]);
-            var html = "<li><div class='row'><div class='small-1 columns'><div class='history_date'>" + msg.content[i].date + "</div></div><div class='small-9 columns'><div class='history_payment'><div class='history_payment_transaction>" + msg.content[i].description + "</div><div class='history_payment_category'>" + msg.content[i].category + "</div></div></div><div class='small-2 columns'><div class='history_value'>" + msg.content[i].amount + " " + msg.content[i].currency + "</div></div></div></li>";
+            function compare (a,b) {
+                if (a.date < b.date)
+                    return 1;
+                if (a.date > b.date)
+                    return -1;
+                return 0;
+            }
+            msg.content.sort(compare);          // funkcja sortująca tablicę od największych do najmniejszych dat
 
-            $(".list").append(html);
-        }
+            for (i=0; i < msg.content.length; i++) {
+
+                console.log(msg.content[i]);
+                var apiDateEdited = msg.content[i].date.substr(0,10).split("-").reverse().join(".");
+                console.log("zmienna apiDateEdited:");
+                console.log(apiDateEdited);
+
+
+                var html = "<li><div class='row'><div class='small-2 columns'><div class='history_date'>" + apiDateEdited + "</div></div>" +
+                    "<div class='small-7 columns'>" +
+                    "<div class='history_payment'>" +
+                    "<div class='history_payment_transaction'>" + msg.content[i].description + "</div>" +
+                    "<div class='history_payment_category'>" + msg.content[i].category + "</div>" +
+                    "   </div>" +
+                    "</div>" +
+                    "<div class='small-3 columns'><div class='history_value'>" + msg.content[i].amount + " " + msg.content[i].currency + "</div></div></div></li>";
+
+                $(".list").append(html);
+            }
 
         }
     );
