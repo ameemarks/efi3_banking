b9b6ca18c6c6d342b56de0252a0f499876ee8a50
Anna Grochola <grochola.anna@gmail.com>
2016-09-12 02:15:58 +0200

    ready bez aria, mixinów i gulpa

---
+++ js/button_unfinished.js
@@ -0,0 +1,34 @@
+/**
+ * Created by ania on 9/11/16.
+ */
+
+$(document).ready (function() {
+    var buttonAll = document.getElementById("button_all");
+    var buttonUnfinished = document.getElementById("button_unfinished");
+    var optionsElem = document.getElementsByClassName("account_options_elem");
+    var awardsElem = document.getElementsByClassName("awards_elem");
+
+    function klikanie (button, elem, hidden) {
+
+        $(button).on("click", function(event){
+            event.preventDefault();
+
+            for (i=0; i < elem.length; i++) {
+
+                if (elem[i].dataset.unfinished == "false") {
+                    if (hidden) {
+                        $(elem[i]).fadeOut(1000);        //addClass("hidden")
+                    }
+                    else {
+                        $(elem[i]).fadeIn(1000);        //removeClass("hidden")
+                    }
+                }
+            }
+        });
+    }
+
+    klikanie (buttonAll, optionsElem, false);
+    klikanie (buttonUnfinished, optionsElem, true);
+    klikanie (buttonAll, awardsElem, false);
+    klikanie (buttonUnfinished, awardsElem, true);
+});
