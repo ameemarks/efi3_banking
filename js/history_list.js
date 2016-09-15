51a6e3e9cc9a3ae8adf861a7dcc108ca884276d2
Anna Grochola <grochola.anna@gmail.com>
2016-09-15 23:27:01 +0200

    chart in dashboard ready

--- js/history_list.js
+++ js/history_list.js
@@ -16,42 +16,35 @@ $(document).ready(function() {
             });
     }
 
-
     ajaxPost(null, "data/history", "GET", function (msg) {
-            console.log("Poniżej jest msg:");
-            console.log(msg);
 
-            console.log("poniżej jest tablica msg.content z obiektami:");
-            console.log(msg.content);
+        function compare (a,b) {
+            if (a.date < b.date)
+                return 1;
+            if (a.date > b.date)
+                return -1;
+            return 0;
+        }
 
-            function compare (a,b) {
-                if (a.date < b.date)
-                    return 1;
-                if (a.date > b.date)
-                    return -1;
-                return 0;
-            }
-            msg.content.sort(compare);          // funkcja sortująca tablicę od największych do najmniejszych dat
+        /*funkcja sortująca tablicę od największych do najmniejszych dat*/
+        msg.content.sort(compare);
 
-            for (i=0; i < msg.content.length; i++) {
+        for (i=0; i < msg.content.length; i++) {
 
-                console.log(msg.content[i]);
-                var apiDateEdited = msg.content[i].date.substr(0,10).split("-").reverse().join(".");
-                console.log("zmienna apiDateEdited:");
-                console.log(apiDateEdited);
+            /* zmienna tworząca datę w odpowiednim formacie do wypchnięcia na stronę*/
+            var apiDateEdited = msg.content[i].date.substr(0,10).split("-").reverse().join(".");
 
+            var html = "<li class='small-12 columns'><div class='row collapse history_elem'><div class='small-2 columns'><div class='history_date'>" + apiDateEdited + "</div></div>" +
+                "<div class='small-7 columns'>" +
+                "<div class='history_payment'>" +
+                "<div class='history_payment_transaction'>" + msg.content[i].description + "</div>" +
+                "<div class='history_payment_category'>" + msg.content[i].category + "</div>" +
+                "   </div>" +
+                "</div>" +
+                "<div class='small-3 columns'><div class='history_value'><span>" + msg.content[i].amount +"</span> " + msg.content[i].currency + "</div></div></div></li>";
 
-                var html = "<li class='small-12 columns'><div class='row collapse history_elem'><div class='small-2 columns'><div class='history_date'>" + apiDateEdited + "</div></div>" +
-                    "<div class='small-7 columns'>" +
-                    "<div class='history_payment'>" +
-                    "<div class='history_payment_transaction'>" + msg.content[i].description + "</div>" +
-                    "<div class='history_payment_category'>" + msg.content[i].category + "</div>" +
-                    "   </div>" +
-                    "</div>" +
-                    "<div class='small-3 columns'><div class='history_value'><span>" + msg.content[i].amount +"</span> " + msg.content[i].currency + "</div></div></div></li>";
-
-                $(".list").append(html);
-            }
+            $(".list").append(html);
+        }
 
         }
     );
